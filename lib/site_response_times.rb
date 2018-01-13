3a4ec06a528482fd0952a6714d9bd692d132651a
em-gazelle <emmaf9@gmail.com>
2018-01-13 14:35:51 -0500

    Accessing thor commands via ruby executable

--- lib/site_response_times.rb
+++ lib/site_response_times.rb
@@ -2,32 +2,57 @@ require "site_response_times/version"
 require 'benchmark'
 require 'uri'
 require 'net/http'
+require 'thor'
 
 module SiteResponseTimes
-	@total_response_time = 0
-	@times_to_run = 10
-	@site_to_check = "https://about.gitlab.com"
-	@seconds_between_status_checks = 10
+	class ResponseTime < Thor 
+		
+		desc "average url:string seconds_between_requests:integer times_to_run:integer", "Calculates average response time from making an HTTP GET request to any url (default about.gitlab.com) X number of times (default is 6) Y seconds apart (default is 10)."
+		def average(*args)
+			begin
+				params = args.map{|arg| (arg.downcase.partition(":").values_at(0,2))}.to_h
 
+				total_response_time = 0
+				times_to_run = validate_positive_integer(params["times_to_run"]) || 6
+				seconds_between_requests = validate_positive_integer(params["seconds_between_requests"]) || 10
+				
+				url = params["url"] || "https://about.gitlab.com"
+				puts "We're getting #{times_to_run} different requests #{seconds_between_requests} seconds apart to find an accurate average response time from #{url}. This process requires about #{times_to_run * seconds_between_requests} seconds."
 
-	def self.check_gitlab
-		uri = URI(@site_to_check)
+				uri = URI(url)
 
-		@times_to_run.times do
-			response_time = get_response_time(uri)
-			@total_response_time += response_time
+				times_to_run.times do
+					response_time = get_response_time(uri)
+					total_response_time += response_time
 
-			sleep(@seconds_between_status_checks - response_time)
+					sleep(seconds_between_requests - response_time) unless seconds_between_requests.zero?
+				end
+
+				puts "Average site response time from #{url} is #{(total_response_time / times_to_run.to_f)} seconds"
+			rescue
+				puts "Oops, looks like we can't submit a valid GET request to the url you provided. Please provide a proper URL or omit the URL to use the default [https://about.gitlab.com]"
+			end
 		end
 
-		@total_response_time / @times_to_run.to_f
+
+		private
+
+		def get_response_time(uri)
+			Benchmark.measure { Net::HTTP.get(uri) }.real
+		end
+
+		def validate_positive_integer(string_number=nil)
+			if string_number.nil?
+				nil
+			elsif string_number.match(/^[0-9]+$/) && string_number.to_i >= 0
+				string_number.to_i
+			else
+				puts "Invalid parameter - using default"
+				nil
+			end
+		end
+
 	end
-
-
-	private
-
-	def self.get_response_time(uri)
-		Benchmark.measure { Net::HTTP.get(uri) }.real
-	end
-
 end
+
+SiteResponseTimes::ResponseTime.start(argv)
